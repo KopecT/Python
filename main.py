23599a79a78b74d7bb67e2ab6eec68bc4f54265b
KopecT <128178035+KopecT@users.noreply.github.com>
2024-05-14 20:51:31 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,76 @@
+from turtle import Screen, Turtle
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+is_on = True
+
+while is_on:
+    screen.update()
+    time.sleep(.1)
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.score_update()
+
+    if snake.head.xcor() > 300 or snake.head.xcor() < -300 or snake.head.ycor() > 300 or snake.head.ycor() < -300:
+        scoreboard.reset()
+        snake.reset()
+
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
+
+
+screen.exitonclick()
+
+
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class RatingsBreakdown(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_ratings,
+                   reducer=self.reducer_count_ratings),
+            MRStep(reducer=self.reducer_sorted_output)
+         ]
+
+    def mapper_get_ratings(self, _, line):
+        (userID, movieID, rating, timestamp) = line.split('\t')
+        yield movieID, 1
+
+    def reducer_count_ratings(self, key, values):
+        yield str(sum(values)).zfill(5), key
+
+    def reducer_sorted_output(self, count, movies):
+        for movie in movies:
+           yield movie, count
+
+if __name__ == '__main__':
+    RatingsBreakdown.run()
+
+
---
+++ scoreboard.py
@@ -0,0 +1,41 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Arial", 24, "normal")
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+        self.color("white")
+        self.penup()
+        self.goto(x=0, y=270)
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.update_scoreboard()
+        self.hideturtle()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode = "w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+    def score_update(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+
